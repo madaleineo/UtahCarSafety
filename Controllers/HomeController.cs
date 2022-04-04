e751a8728fe33444b6ba88d87443efd5bae20ad8
tylerharris803 <tylerharris803@gmail.com>
2022-04-04 12:10:40 -0600

    login route

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,10 +28,9 @@ namespace UtahCarSafety.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Login()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
